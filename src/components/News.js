18c79b9f25a27c406e46a92552037a5506fe7c0c
JSGulhane <jinalsgulhane@gmail.com>
2024-06-10 02:05:33 +0530

    mapping thru articles arr

--- src/components/News.js
+++ src/components/News.js
@@ -57,20 +57,14 @@ export class News extends Component {
   render() {
     return (
       <div className='container my-3'>
-        <h2>NewMonkey Top headlines</h2>
+        <h2>NewsMonkey Top headlines</h2>
         <div className="row">
-            <div className="col-md-4">
-                 <NewsItem title = "myTitle" description="mydesc" imageUrl="https://cdn.24.co.za/files/Cms/General/d/10743/97d776dc91734e98906c0e1b7f3b1afa.jpg" newsUrl="LOADING"/>
-            </div>
 
-            <div className="col-md-4">
-                <NewsItem title = "myTitle" description="mydesc"/>
+        {this.state.articles.map((element)=>{
+            return <div className="col-md-4" key={element.url}>
+                 <NewsItem title = {element.title.slice(0,45)} description= {element.description.slice(0,88)} imageUrl= {element.urlToImage} newsUrl = {element.url}/>
             </div>
-
-            <div className="col-md-4">
-                <NewsItem title = "myTitle" description="mydesc"/>
-            </div>
- 
+        })}
         </div>
          
       </div>
