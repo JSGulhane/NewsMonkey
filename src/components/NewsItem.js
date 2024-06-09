bad57c67d8f33e39b9edfc1d1f3c4a2ad0f39450
JSGulhane <jinalsgulhane@gmail.com>
2024-06-10 03:14:41 +0530

    fetching thru api directly-endpoint

--- src/components/NewsItem.js
+++ src/components/NewsItem.js
@@ -7,10 +7,10 @@ export class NewsItem extends Component {
     return (
       <div className='my-3'>
         <div className="card" style={{width: "18rem"}}>
-            <img src={imageUrl} className="card-img-top" alt="..."/>
+            <img src={!imageUrl?"https://www.hindustantimes.com/ht-img/img/2024/06/08/550x309/France-Tennis-French-Open-52_1717856814928_1717856858927.jpg":imageUrl} className="card-img-top" alt="..."/>
             <div className="card-body">
                 <h5 className="card-title">{title}...</h5>
-                <p className="card-text">{description}...</p>
+                <p className="card-text">{description}...read more</p>
                 <a href={newsUrl} target= "_blank" rel="noreferrer noopener" className="btn btn-sm btn-primary">Read more</a>
         </div>
         </div>
